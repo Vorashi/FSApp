e97a29d1bcf82b0c3e2e7cf1bf58e2f8ab9c13dd
matvei <[matvejsarov673@gmail.com]>
2025-02-12 10:48:08 +0300

    Изменил код привязав к одной директории

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = '3000';
 
-app.use(express.static(__dirname + '/dataFile'));
+app.use(bodyParser.json());
 
-//http://localhost:3000/files?path=/path/to/your/folder
-app.get('/files', (req, res)=> {
-    const folderPath = req.query.path;
+const folderPath = 'D:/practicSharov/fsApp/dataFile';
 
-    if (!folderPath) return res.status(400).send('Не указан путь к папке');
-
+app.get('/files', (req, res) => {
     if (!fs.existsSync(folderPath)) return res.status(404).send('Указанная папка не существует');
 
-    fs.readdirSync(folderPath, (err, files) => {
+    fs.readdir(folderPath, (err, files) => {
         if (err) return res.status(500).send('Ошибка при чтении папки');
 
         const fileList = files.filter(file => {
@@ -27,9 +25,11 @@ app.get('/files', (req, res)=> {
 });
 
 app.get('/file', (req, res) => {
-    const filePath = req.query.path;
+    const fileName = req.query.name;
 
-    if (!filePath) return res.status(400).send('Не указан путь к файлу');
+    if (!fileName) return res.status(400).send('Не указано имя файла');
+
+    const filePath = path.join(folderPath, fileName);
 
     if (!fs.existsSync(filePath)) return res.status(404).send('Файл не существует');
 
@@ -40,10 +40,12 @@ app.get('/file', (req, res) => {
 });
 
 app.post('/file', (req, res) => {
-    const filePath = req.body.path;
+    const fileName = req.body.name;
     const content = req.body.content;
 
-    if (!filePath || !content) return res.status(400).send('Не указан путь к файлу или содержимое');
+    if (!fileName || !content) return res.status(400).send('Не указано имя файла или содержимое');
+
+    const filePath = path.join(folderPath, fileName);
 
     fs.writeFile(filePath, content, 'utf8', (err) => {
         if (err) return res.status(500).send('Ошибка при сохранении файла');
@@ -52,18 +54,20 @@ app.post('/file', (req, res) => {
 });
 
 app.delete('/file', (req, res) => {
-    const filePath = req.body.path;
+    const fileName = req.body.name;
 
-    if (!filePath) return res.status(400).send('Не указан путь к файлу');
+    if (!fileName) return res.status(400).send('Не указано имя файла');
+
+    const filePath = path.join(folderPath, fileName);
 
     if (!fs.existsSync(filePath)) return res.status(404).send('Файл не существует');
-    
+
     fs.unlink(filePath, (err) => {
         if (err) return res.status(500).send('Ошибка при удалении файла');
         res.send('Файл успешно удален');
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server run on http://localhost:${PORT}! \nLet's go to work`)
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
 });
